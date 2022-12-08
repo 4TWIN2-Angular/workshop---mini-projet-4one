efdd74ac44502422cb9d1b132e4fb6b12289d834
Dhia Bejaoui <bejaoui.dhia@esprit.tn>
2022-12-08 18:01:35 +0100

    merging with zack

--- src/app/services/equipe.service.ts
+++ src/app/services/equipe.service.ts
@@ -1,63 +1,45 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Equipe } from '../Model/equipe';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Equipe } from "../models/equipe";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EquipeService {
+  accountUrl = "http://localhost:9090";
 
-  accountUrl="http://localhost:9090";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  public getEquipes():Observable<Equipe[]>{
+  public getEquipes(): Observable<Equipe[]> {
     console.log("service appelé");
 
-    return this.http.get<Equipe[]>(this.accountUrl+"/getAllEquipes");
-    
-
+    return this.http.get<Equipe[]>(this.accountUrl + "/getAllEquipes");
   }
 
-  public addEquipe(equipe:any){
-
-    return this.http.post(this.accountUrl+"/addEquipe",equipe);
-
-
+  public addEquipe(equipe: any) {
+    return this.http.post(this.accountUrl + "/addEquipe", equipe);
   }
 
-  deleteEquipe(id:any){
-
-    return this.http.delete(this.accountUrl+"/removeEquipe/"+id);
+  deleteEquipe(id: any) {
+    return this.http.delete(this.accountUrl + "/removeEquipe/" + id);
   }
 
-  updateEquipe(equipe:any)
-  {
-    return this.http.put(this.accountUrl+"/updateEquipe",equipe);
-
+  updateEquipe(equipe: any) {
+    return this.http.put(this.accountUrl + "/updateEquipe", equipe);
   }
-  getEquipe(id:any){
-
-
-    return this.http.get(this.accountUrl+"/getEquipe/"+id);
-
+  getEquipe(id: any) {
+    return this.http.get(this.accountUrl + "/getEquipe/" + id);
   }
 
-  upgradeEquipe()
-  {
-    return this.http.get(this.accountUrl+"/upgradeEquipe");
-
+  upgradeEquipe() {
+    return this.http.get(this.accountUrl + "/upgradeEquipe");
   }
-  getEtudiantsByEquipe(id:any)
-  {
-    return this.http.get(this.accountUrl+"/getEtudiantsByEquipe/"+id);
-
+  getEtudiantsByEquipe(id: any) {
+    return this.http.get(this.accountUrl + "/getEtudiantsByEquipe/" + id);
   }
 
-  countEquipeByNiveau(niveau:any)
-  {
-    return this.http.get(this.accountUrl+"/countByNiveau/"+niveau);
+  countEquipeByNiveau(niveau: any) {
+    return this.http.get(this.accountUrl + "/countByNiveau/" + niveau);
   }
-
 }
