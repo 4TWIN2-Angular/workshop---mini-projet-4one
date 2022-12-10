5db142423deda857c71891faec85aef04ea29b59
Dhia Bejaoui <bejaoui.dhia@esprit.tn>
2022-12-10 15:19:14 +0100

    changes

--- src/app/pages/add-equipe/add-equipe.component.ts
+++ src/app/pages/add-equipe/add-equipe.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { EquipeService } from 'src/app/services/equipe.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { EquipeService } from "src/app/services/equipe.service";
 
 @Component({
-  selector: 'app-add-equipe',
-  templateUrl: './add-equipe.component.html',
-  styleUrls: ['./add-equipe.component.css']
+  selector: "app-add-equipe",
+  templateUrl: "./add-equipe.component.html",
+  styleUrls: ["./add-equipe.component.css"],
 })
 export class AddEquipeComponent implements OnInit {
-  equipeData={
-    nomEquipe:'',
-    niveau:'',
+  equipeData = {
+    nomEquipe: "",
+    niveau: "",
   };
-  constructor(private equipeService:EquipeService) { }
+  constructor(private equipeService: EquipeService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addEquipe() {
+    this.equipeService.addEquipe(this.equipeData).subscribe(() => {
+      this.router.navigate(["equipes"]);
+    });
   }
-
-  addEquipe()
-  {
-    this.equipeService.addEquipe(this.equipeData).subscribe();
-
-  }
-
 }
--- src/app/pages/update-equipe/update-equipe.component.ts
+++ src/app/pages/update-equipe/update-equipe.component.ts
@@ -1,31 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { EquipeService } from 'src/app/services/equipe.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { EquipeService } from "src/app/services/equipe.service";
 
 @Component({
-  selector: 'app-update-equipe',
-  templateUrl: './update-equipe.component.html',
-  styleUrls: ['./update-equipe.component.css']
+  selector: "app-update-equipe",
+  templateUrl: "./update-equipe.component.html",
+  styleUrls: ["./update-equipe.component.css"],
 })
 export class UpdateEquipeComponent implements OnInit {
-  id=0;
-  equipe:any;
-  constructor(private equipeService:EquipeService,private route:ActivatedRoute, private router : Router) { }
+  id = 0;
+  equipe: any;
+  constructor(
+    private equipeService: EquipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['idEquipe'];
+    this.id = this.route.snapshot.params["idEquipe"];
     console.log(this.id);
 
-    this.equipeService.getEquipe(this.id).subscribe(res=>this.equipe=res);
+    this.equipeService
+      .getEquipe(this.id)
+      .subscribe((res) => (this.equipe = res));
     console.log(this.equipe);
-     
   }
-  updateEquipe()
-  {
-    this.equipeService.updateEquipe(this.equipe).subscribe();
-
+  updateEquipe() {
+    this.equipeService.updateEquipe(this.equipe).subscribe(() => {
+      this.router.navigate(["equipes"]);
+    });
   }
-
-  
-
 }
